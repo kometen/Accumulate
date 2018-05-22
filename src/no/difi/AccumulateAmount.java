dd48a3c1388e9033aaa6ad9c15344acce367b01e
Claus Guttesen <kometen@gmail.com>
2018-05-22 20:15:54 +0200

    Accumulate YTD

--- src/no/difi/AccumulateAmount.java
+++ src/no/difi/AccumulateAmount.java
@@ -1,33 +1,33 @@
 package no.difi;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Map.Entry;
 
-import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
 import com.google.common.collect.TreeBasedTable;
 
 public class AccumulateAmount {
 
     public static void main(String[] args) {
-        Map<String, Map<Integer, Integer>> records = new HashMap<>();
-        Map<Integer, Integer> amount = new HashMap<>();
-        amount.put(5, 5);
-        amount.put(3, 3);
-        amount.put(4, 4);
-        amount.put(1, 1);
-        amount.put(11, 11);
-        amount.put(2, 2);
-        records.put("Formidling af viden", amount);
-
-        records.forEach((k1, v1) -> {
-            System.out.println("primary key: " + k1 + ", value: " + v1);
-            v1.forEach((k2, v2) -> {
-                System.out.println("period: " + k2 + ", value: " + v2);
-            });
-        });
-        
+    	Integer ytd = 0; // Year To Date
         Table<String, Integer, Integer> tAmount = TreeBasedTable.create();
-    }
+        
+        tAmount.put("Formidling af viden", 5, 5);
+        tAmount.put("Formidling af viden", 1, 1);
+        tAmount.put("Motivering vha. podcast", 1, 1);
+        tAmount.put("Formidling af viden", 4, 4);
+        tAmount.put("Formidling af viden", 2, 2);
+        tAmount.put("Formidling af viden", 11, 11);
+        tAmount.put("Motivering vha. podcast", 2, 2);
+        tAmount.put("Formidling af viden", 3, 3);
+        System.out.println("guava table: " + tAmount.size());
 
+        for (String item : tAmount.rowKeySet()) {
+            System.out.println("guava rowKeySet(): " + item);
+            ytd = 0;
+            for (Entry<Integer, Integer> period : tAmount.row(item).entrySet()) {
+            	ytd += period.getValue();
+                System.out.println("period: " + period.getKey() + ", value: " + period.getValue() + ", YTD: " + ytd);
+            }
+        }
+    }
 }
